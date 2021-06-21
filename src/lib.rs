fe3cdd3d20a7e43ca6711c10cff9a19fa16aec9b
Eric Zhao <21zhaoe@protonmail.com>
2021-06-21 19:15:01 -0400

    Add tests for Perm methods

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,8 @@ impl Perm {
     #[inline]
     pub fn as_sym(&self) -> String {
         let r = if self.read { "r" } else { "" };
-        let w = if self.read { "w" } else { "" };
-        let x = if self.read { "x" } else { "" };
+        let w = if self.write { "w" } else { "" };
+        let x = if self.execute { "x" } else { "" };
         format!("{}{}{}", r, w, x)
     }
 
@@ -102,5 +102,66 @@ fn diff(a: bool, b: bool) -> DiffOp {
         }
     } else {
         Same
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! perm {
+        ($r:expr, $w:expr, $x:expr) => {
+            Perm {
+                read: $r,
+                write: $w,
+                execute: $x,
+            }
+        };
+    }
+
+    #[test]
+    fn test_perm_num() {
+        macro_rules! perm_num {
+            ($r:expr, $w:expr, $x:expr) => {
+                perm!($r, $w, $x).as_num()
+            };
+        }
+
+        macro_rules! test_perm_num {
+            ($c:expr, $r: expr, $w:expr, $x:expr) => {
+                assert_eq!($c, perm_num!($r, $w, $x))
+            };
+        }
+
+        test_perm_num!(7, true, true, true);
+        test_perm_num!(6, true, true, false);
+        test_perm_num!(5, true, false, true);
+        test_perm_num!(4, true, false, false);
+        test_perm_num!(3, false, true, true);
+        test_perm_num!(2, false, true, false);
+        test_perm_num!(1, false, false, true);
+    }
+
+    #[test]
+    fn test_perm_sym() {
+        macro_rules! perm_sym {
+            ($r:expr, $w:expr, $x:expr) => {
+                perm!($r, $w, $x).as_sym()
+            };
+        }
+
+        macro_rules! test_perm_sym {
+            ($c:expr, $r: expr, $w:expr, $x:expr) => {
+                assert_eq!($c, perm_sym!($r, $w, $x))
+            };
+        }
+
+        test_perm_sym!("rwx", true, true, true);
+        test_perm_sym!("rw", true, true, false);
+        test_perm_sym!("rx", true, false, true);
+        test_perm_sym!("r", true, false, false);
+        test_perm_sym!("wx", false, true, true);
+        test_perm_sym!("w", false, true, false);
+        test_perm_sym!("x", false, false, true);
     }
 }
