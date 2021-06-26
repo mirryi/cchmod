5f6e25e2df69a48e12c7857f73da74d9412177f6
Eric Zhao <21zhaoe@protonmail.com>
2021-06-25 22:03:34 -0400

    doc: Add docs for perm constants

--- src/perm.rs
+++ src/perm.rs
@@ -1,20 +1,28 @@
 use crate::Perm;
 
 macro_rules! const_perm {
-    ($name:ident, $read:expr, $write:expr, $execute:expr) => {
-        pub const $name: Perm = Perm {
-            read: $read,
-            write: $write,
-            execute: $execute,
-        };
+    ($name:ident, $s:expr, $read:expr, $write:expr, $execute:expr) => {
+        doc_comment::doc_comment! {
+            concat!("Permission `", $s, "`, equivalent to",
+                    " `Perm { read: ", stringify!($read), ",",
+                            " write: ", stringify!($write), ",",
+                            " execute: ", stringify!($execute),
+                            " }",
+                    "`."),
+            pub const $name: Perm = Perm {
+                read: $read,
+                write: $write,
+                execute: $execute,
+            };
+        }
     };
 }
 
-const_perm!(_7, true, true, true);
-const_perm!(_6, true, true, false);
-const_perm!(_5, true, false, true);
-const_perm!(_4, true, false, false);
-const_perm!(_3, false, true, true);
-const_perm!(_2, false, true, false);
-const_perm!(_1, false, false, true);
-const_perm!(_0, false, false, false);
+const_perm!(_7, "rwx", true, true, true);
+const_perm!(_6, "rw-", true, true, false);
+const_perm!(_5, "r-x", true, false, true);
+const_perm!(_4, "r--", true, false, false);
+const_perm!(_3, "-wx", false, true, true);
+const_perm!(_2, "-w-", false, true, false);
+const_perm!(_1, "--x", false, false, true);
+const_perm!(_0, "---", false, false, false);
